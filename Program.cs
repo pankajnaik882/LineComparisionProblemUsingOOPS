41f100143f5463b95065fa4df0b5512a81b4df21
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 18:59:54 +0530

    Add : Checking Equality Of Two Lines

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            FanOfGeometry i = new FanOfGeometry();
-            i.fanOfGeometry();
+            FanOfGeometry el = new FanOfGeometry();
+            el.fanOfGeometry();
         }
     }
 }
