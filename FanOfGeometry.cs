0783b2890d3eab1972c471f03824940130dc5b75
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 18:45:50 +0530

    Add : Input Line Coordinates From User

---
+++ FanOfGeometry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblemUsingOOPS
+{
+    public class FanOfGeometry
+    {
+        public void fanOfGeometry()
+        {
+
+            Console.WriteLine("Enter the x1 co-ordinate");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the y1 co-ordinate");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the x2 co-ordinate");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the y2 co-ordinate");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The lenght of line using co-ordinate : " + lengthOfLine);
+        }
+    }
+}
