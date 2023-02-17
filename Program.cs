7843c267bbb573316df5d2b841a3c27e73451745
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 18:45:26 +0530

    Add : Input Line Coordinates From User

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Line Comparision Problem Using OOPs");
+            FanOfGeometry i = new FanOfGeometry();
+            i.fanOfGeometry();
         }
     }
 }
