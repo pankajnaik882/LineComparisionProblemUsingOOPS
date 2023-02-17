9342e2ff31e0f6c41f3a163bd0a218cece3e720d
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 19:10:56 +0530

    Add : Comparing Two Lines

--- FanOfGeometry.cs
+++ FanOfGeometry.cs
@@ -10,7 +10,6 @@ namespace LineComparisionProblemUsingOOPS
     {
         public void fanOfGeometry()
         {
-
             Console.WriteLine("Enter the x1 co-ordinate");
             double x1 = Convert.ToDouble(Console.ReadLine());
 
@@ -26,10 +25,18 @@ namespace LineComparisionProblemUsingOOPS
             double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             Console.WriteLine("The lenght of line using co-ordinate : " + lengthOfLine);
 
-            bool checkX = Math.Equals(x2 - x1, lengthOfLine);
-            bool checkY = Math.Equals(y2 - y1, lengthOfLine);
-            Console.WriteLine(checkX);
-            Console.WriteLine(checkY);
+            if (x1 > x2 && y1 > y2)
+            {
+                Console.WriteLine("X1 and Y1 is greater than X2 and Y2");
+            }
+            else if (x2 > x1 && y2 > y1)
+            {
+                Console.WriteLine("X2 And Y2 is greater than X1 and Y1");
+            }
+            else
+            {
+                Console.WriteLine("Both Are Equal");
+            }
         }
     }
 }
