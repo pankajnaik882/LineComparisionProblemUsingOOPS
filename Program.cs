acc583fee610dd8c2dca642641a7a25d5bda60b3
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 18:31:59 +0530

    Add : Welcome to Line Comparision Problem Using OOPS

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisionProblemUsingOOPS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparision Problem Using OOPs");
+        }
+    }
+}
